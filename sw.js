0d6e2cb248628e9156187b3aeeca5468c6aa3a6f
jeyaram1023 <jeyaram.reddy.ece@gmail.com>
2025-06-08 22:05:12 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,9 @@
 const CACHE_NAME = 'streetr-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
+  '/Customer-app-Testing-/',
+  '/Customer-app-Testing-/index.html',
+  '/Customer-app-Testing-/manifest.json',
+  '/Customer-app-Testing-/icons/icon-192.png',
+  '/Customer-app-Testing-/icons/icon-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
-
-// Install the service worker
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// Activate the service worker
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      )
-    )
-  );
-});
-
-// Fetch requests
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
-  );
-});
